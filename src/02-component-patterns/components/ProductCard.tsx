476292e0d36018076ba1b01e9acecfc6f99b716e
cvillalobos2 <cvillalobos@soin.co.cr>
2022-01-05 17:18:03 -0600

    ProductCard

---
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -0,0 +1,22 @@
+import styles from '../styles/styles.module.css';
+import noImage from '../assets/no-image.jpg';
+
+export const ProductCard = () => {
+    return (
+        <div className={styles.productCard}>
+            {/* <img className={styles.productImg} src='./coffee-mug.png' alt='Cofffe Mug' /> */}
+            <img className={styles.productImg} src={noImage} alt='Cofffe Mug' />
+            <span className={styles.productDescription}>Coffee Mug</span>
+            <div className={styles.buttonsContainer}>
+
+                <button className={styles.buttonMinus}> - </button>
+
+                <div className={styles.countLabel}>
+                    0
+                </div>
+
+                <button className={styles.buttonAdd}> + </button>
+            </div>
+        </div>
+    )
+}
