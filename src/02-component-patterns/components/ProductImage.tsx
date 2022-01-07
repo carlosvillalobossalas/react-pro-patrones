78a8bff4e69ef27ce4aa304e17c49dbb751ab10d
cvillalobos2 <cvillalobos@soin.co.cr>
2022-01-06 20:09:12 -0600

    Component Extend Styles listo

--- src/02-component-patterns/components/ProductImage.tsx
+++ src/02-component-patterns/components/ProductImage.tsx
@@ -1,9 +1,15 @@
-import { useContext } from 'react';
+import { CSSProperties, useContext } from 'react';
 import { ProductContext } from './ProductCard';
 import styles from '../styles/styles.module.css';
 import noImage from '../assets/no-image.jpg';
 
-export const ProductImage = ({ img = '' }) => {
+export interface Props {
+    className?: string;
+    img?: string;
+    style?: CSSProperties;
+}
+
+export const ProductImage = ({ img, className, style }: Props) => {
 
     const { product } = useContext(ProductContext);
 
@@ -11,6 +17,11 @@ export const ProductImage = ({ img = '' }) => {
     imgToShow = img ? img : (product.img ? product.img : noImage);
 
     return (
-        <img className={styles.productImg} src={imgToShow} alt='Product Pic' />
+        <img
+            alt='Product Pic'
+            className={`${styles.productImg} ${className}`}
+            src={imgToShow}
+            style={style}
+        />
     )
 }
--- src/02-component-patterns/components/ProductTitle.tsx
+++ src/02-component-patterns/components/ProductTitle.tsx
@@ -1,8 +1,14 @@
-import { useContext } from 'react';
+import { CSSProperties, useContext } from 'react';
 import { ProductContext } from './ProductCard';
 import styles from '../styles/styles.module.css';
 
-export const ProductTitle = ({ title }: { title?: string }) => {
+export interface Props {
+    className?: string;
+    title?: string;
+    style?: CSSProperties;
+}
+
+export const ProductTitle = ({ title, className, style }: Props) => {
 
     const { product } = useContext(ProductContext);
 
@@ -10,6 +16,11 @@ export const ProductTitle = ({ title }: { title?: string }) => {
     titleToShow = title ? title : product.title;
 
     return (
-        <span className={styles.productDescription}>{titleToShow}</span>
+        <span
+            className={`${styles.productDescription} ${className}`}
+            style={style}
+        >
+            {titleToShow}
+        </span>
     )
 }
