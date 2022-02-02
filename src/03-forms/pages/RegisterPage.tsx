d2b267c5ffac445ae5baca19e48e515d3dd3cc32
cvillalobos2 <cvillalobos@soin.co.cr>
2022-02-01 21:06:46 -0600

    form - components - fields - added

---
+++ src/03-forms/pages/RegisterPage.tsx
@@ -0,0 +1,67 @@
+import { FormEvent } from 'react';
+import { useForm } from '../hooks/useForm';
+import '../styles/styles.css';
+
+export const RegisterPage = () => {
+
+    const { formData, onChange, name, email, password1, password2, resetForm, isValidEmail } = useForm({
+        name: '',
+        email: '',
+        password1: '',
+        password2: ''
+    });
+
+    const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log(formData);
+    }
+
+    return (
+        <div>
+            <h1>Register Page</h1>
+            <form noValidate onSubmit={onSubmit}>
+                <input
+                    name='name'
+                    placeholder="Name"
+                    type="text"
+                    value={name}
+                    onChange={onChange}
+                    className={`${name.trim().length <= 0 && 'has-error'}`}
+                />
+                {name.trim().length <= 0 && <span>Este campo es necesario</span>}
+
+                <input
+                    name='email'
+                    placeholder="Email"
+                    type="email"
+                    value={email}
+                    onChange={onChange}
+                    className={`${!isValidEmail(email) && 'has-error'}`}
+                />
+                {!isValidEmail(email) && <span>Email no es valido</span>}
+
+                <input
+                    name='password1'
+                    placeholder="Password"
+                    type="password"
+                    value={password1}
+                    onChange={onChange}
+                />
+                {password1.trim().length <= 0 && <span>Este campo es necesario</span>}
+                {password1.trim().length < 6 && password1.trim().length > 0 && <span>La contrasena tiene que ser mas de 6 caracteres</span>}
+
+                <input
+                    name='password2'
+                    placeholder="Repeat Password"
+                    type="password"
+                    value={password2}
+                    onChange={onChange}
+                />
+                {password2.trim().length <= 0 && <span>Este campo es necesario</span>}
+                {password2.trim().length > 0 && password1 !== password2 && <span>Contrasenas deben ser iguales</span>}
+                <button type="submit" >Create</button>
+                <button type='button' onClick={resetForm}>Reset</button>
+            </form>
+        </div>
+    )
+}
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -4,7 +4,7 @@ import {
   Route,
   NavLink
 } from 'react-router-dom';
-
+import { RegisterPage, FormikBasicPage, FormikYupPage, FormikComponents, FormikAbstraction } from '../03-forms/pages';
 import logo from '../logo.svg';
 
 export const Navigation = () => {
@@ -15,10 +15,19 @@ export const Navigation = () => {
           <img src={logo} alt="React Logo" />
           <ul>
             <li>
-              <NavLink to="/home" activeClassName="nav-active" exact>Home</NavLink>
+              <NavLink to="/register" activeClassName="nav-active" exact>Register</NavLink>
             </li>
             <li>
-              <NavLink to="/about" activeClassName="nav-active" exact>About</NavLink>
+              <NavLink to="/formik-basic" activeClassName="nav-active" exact>Formik Basic</NavLink>
+            </li>
+            <li>
+              <NavLink to="/formik-yup" activeClassName="nav-active" exact>Formik Yup</NavLink>
+            </li>
+            <li>
+              <NavLink to="/formik-components" activeClassName="nav-active" exact>Formik Components</NavLink>
+            </li>
+            <li>
+              <NavLink to="/formik-abstraction" activeClassName="nav-active" exact>Formik Abstraction</NavLink>
             </li>
             <li>
               <NavLink to="/users" activeClassName="nav-active" exact>Users</NavLink>
@@ -26,17 +35,24 @@ export const Navigation = () => {
           </ul>
         </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <Switch>
           <Route path="/about">
             <h1>About</h1>
           </Route>
-          <Route path="/users">
-            <h1>Users</h1>
+          <Route path="/formik-basic">
+            <FormikBasicPage />
           </Route>
-          <Route path="/home">
-            <h1>Home</h1>
+          <Route path="/formik-yup">
+            <FormikYupPage />
+          </Route>
+          <Route path="/formik-components">
+            <FormikComponents />
+          </Route>
+          <Route path="/formik-abstraction">
+            <FormikAbstraction />
+          </Route>
+          <Route path="/register">
+            <RegisterPage />
           </Route>
         </Switch>
       </div>
